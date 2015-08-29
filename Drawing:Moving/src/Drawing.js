4631d1dade814fe6ca4a9c15f1c4dd5b7e52f9ae
gaelhugo <gael.hugo@onemore-studio.com>
2015-08-29 17:06:08 +0200

    BlendModeClock
    Added Simple circle animation with blended

--- Drawing:Moving/src/Drawing.js
+++ Drawing:Moving/src/Drawing.js
@@ -5,6 +5,7 @@ canvas.width = width =  window.innerWidth;
 canvas.height = height = window.innerHeight;
 var ctx = canvas.getContext('2d');
 
+
 //create a circle
 var circle = new Circle(200,200,10,ctx);
 var circle2 = new Circle(400,400,10,ctx);
@@ -18,36 +19,68 @@ var binary;
 var numbers = new Numbers();
 
 
-
-
-var spacer = Math.random()*30 +10;
-var spacerY = Math.random()*30 +10;
+var spacer = 20;//Math.round(Math.random()*30 +10);
+var spacerY = 20;//Math.round(Math.random()*30 +10);
 var letterSpacer = 5;
+var radius = 20;
 
-var posx;
+var posx = width/2 - (10*5*(spacer+letterSpacer))/2;
 var posy = height/2 - 4*spacerY;
+
+var s; // check previous second
+var counter = 0;
+
+// BUFFER OPTION
+var allShapes = [];
+function setup(){
+	//Build all neededParticle for a time model
+	// 12 numbers and sign with milliseconds
+	for(var i=0;i<10;i++){
+			//line
+		for(var j=0;j<7;j++){
+			//colnumber
+			for(var k=0;k<5;k++){
+				//store all shapes only once
+				var shape = new ShapeInMotion(posx + k*spacer,posy + j*spacerY,radius,ctx);
+				allShapes.push(shape);	
+			
+			}
+		}	
+		posx+=5*(spacer+letterSpacer);
+	}
+	
+	draw();
+}
+
 
 function draw(){
 	//console.log("I'm drawing");
 	//clear canvas
 	//cover method
+	ctx.save();
+	ctx.globalCompositeOperation = 'source-over';
+	ctx.globalAlpha = 0.04;
 	ctx.fillStyle = "black";
 	ctx.fillRect(0,0,width,height);
+	ctx.restore();
 	//empty method
 	//ctx.clearRect(0,0,width,height);
 	ctx.fillStyle = "white";
 	//circle.draw();
 	//circle2.draw();
 	//square.drawCenterSquare();
-	
+	ctx.globalCompositeOperation = 'lighter';
 	d = new Date();
 	//console.log(d.getMilliseconds());
 	ctx.font="30px Arial";
-	string = ((d.getHours()<10)?0:'')+d.getHours()+':'+((d.getMinutes()<10)?0:'')+d.getMinutes()+':'+((d.getSeconds()<10)?0:'')+d.getSeconds()+':'+((d.getMilliseconds()<100)?0:'')+d.getMilliseconds();
-	ctx.fillText(string,0,24);
-	
+	string = ((d.getHours()<10)?0:'')+d.getHours()+':'+((d.getMinutes()<10)?0:'')+d.getMinutes()+':'+((d.getSeconds()<10)?0:'')+d.getSeconds()+':'+ Math.floor(d.getMilliseconds()/100);
+	//ctx.fillText(string,0,24);
+	var col = (Math.round(Math.random() * 255)) + ',' + (Math.round(Math.random() * 255)) + ',' + (Math.round(Math.random() * 255));
+	var randomColor = 'rgba(' + col + ',0.009)';
+	var randomColorFull = 'rgba(' + col + ',0.1)';
 	allLetters = string.split("");
-	posx = width/2 - (12*5*(spacer+letterSpacer))/2;
+	counter = 0;
+	var ii = -1;
 	for(var i=0;i<allLetters.length;i++){
 		//console.log(allLetters[i]);
 		//get binary letter composition
@@ -59,15 +92,22 @@ function draw(){
 			for(var k=0;k<binary[j].length;k++){
 				//draw stuff
 				//console.log("draw");
-				if(binary[j][k] == 1){
-					var shape = new Shape(posx + k*spacer,posy + j*spacerY,5,ctx);
-					shape.drawCircle();
+				if(s!=d.getSeconds()){
+					allShapes[counter].reset();
+					allShapes[counter].setColor(randomColor,randomColorFull);
 				}
+				allShapes[counter].updatePosition();
+				if(binary[j][k] == 1){
+					allShapes[counter].drawCircle();
+					allShapes[counter].drawFixCircle();
+				}
+				counter++;
 			}
 		}
-		posx+=5*(spacer+letterSpacer);
+		
+		
 	}
-	
+	s = d.getSeconds();
 	
 	
 	
@@ -75,4 +115,4 @@ function draw(){
 }
 
 //launch a drawing method
-draw();
+setup();
--- Drawing:Moving/src/ShapeInMotion.js
+++ Drawing:Moving/src/ShapeInMotion.js
@@ -1,10 +1,18 @@
 // JavaScript Document
 var ShapeInMotion = function(x,y,radius,ctx){
+	this.originX = x;
 	this.x = x;
+	this.originY = y;
 	this.y = y;
 	this.radius = radius;
+	this.originalRadius = radius;
 	this.ctx = ctx;
-	this.color;
+	this.color = 'white';
+	this.speedX;
+	this.speedY;
+	this.colorFull = 'white';
+	this.radiusAugmentation = Math.random()*2;
+	this.fixRadius = 0;
 }
 
 
@@ -13,22 +21,52 @@ ShapeInMotion.prototype = {
 	drawCircle:function(){
 	  this.ctx.beginPath();
       this.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
-      this.ctx.fillStyle = 'white';
+      this.ctx.fillStyle = this.color
       this.ctx.fill();
+	  this.ctx.closePath();
+	},
+	
+	drawFixCircle:function(){
+	  this.ctx.beginPath();
+      this.ctx.arc(this.originX, this.originY, this.fixRadius , 0, 2 * Math.PI, false);
+      this.ctx.fillStyle = this.colorFull;
+      this.ctx.fill();
+	  this.ctx.closePath();
 	},
 	
 	drawSquare:function(){
 	  this.ctx.beginPath();
       this.ctx.rect(this.x,this.y,this.radius,this.radius);
-      this.ctx.fillStyle = 'white';
+      this.ctx.fillStyle = this.color
       this.ctx.fill();
 	},
 	
 	drawCenterSquare:function(){
 	  this.ctx.beginPath();
       this.ctx.rect(this.x-(this.radius/2),this.y-(this.radius/2),this.radius,this.radius);
-      this.ctx.fillStyle = 'white';
+      this.ctx.fillStyle = this.color;
       this.ctx.fill();
+	},
+	
+	updatePosition:function(){
+		this.x+=this.speedX;
+		this.y+=this.speedY;
+		this.radius+=this.radiusAugmentation ;
+		this.fixRadius  = (this.fixRadius <this.originalRadius)?this.fixRadius+=0.2:this.originalRadius;
+	},
+	
+	reset:function(){
+		this.x = this.originX;
+		this.y = this.originY;
+		this.speedX = Math.round( 1 - Math.random()*2 )* (Math.random()*5);
+		this.speedY = Math.round( 1 - Math.random()*2 )*(Math.random()*10);
+		this.radius = this.originalRadius ;
+		this.fixRadius = 0;
+	},
+	
+	setColor:function(color,colorFull){
+		this.color =color;
+		this.colorFull = colorFull;
 	}
 	
 }
